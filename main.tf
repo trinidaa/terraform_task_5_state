3d69134fb2d04da20b0ef4529e6ecdcc8e158071
Viacheslav Kaliak <trinidaa@gmial.com>
2025-10-21 10:43:03 +0200

    solution 3

--- main.tf
+++ main.tf
@@ -18,7 +18,15 @@ terraform {
 
 
 provider "azurerm" {
-  features {}
-  use_oidc        = true
-  subscription_id = var.subscription_id
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+
+resource "azurerm_resource_group" "rgp" {
+  name     = "tfstate"
+  location = "East US"
 }
